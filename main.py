745a3e1e00eb0413f534524cba5300ad175d024d
John Zlotek <jzlotek@gmail.com>
2020-05-23 17:04:06 -0400

    test suite and refactor

--- main.py
+++ main.py
@@ -6,93 +6,15 @@ import time
 
 from pygame import K_DOWN, K_LEFT, K_UP, K_RIGHT, K_KP_ENTER
 
+from board import Board
+
 pygame.init()
 
 size = width, height = 640, 480
 
-BLOCK_SIZE = 20
-
-PIECES = [
-    (colors.YELLOW, np.array([[1, 1], [1, 1]])),
-    (colors.RED, np.array([[1, 1, 0], [0, 1, 1]])),
-    (colors.GREEN, np.array([[0, 1, 1], [1, 1, 0]])),
-    (colors.ORANGE, np.array([[0, 0, 1], [1, 1, 1]])),
-    (colors.BLUE, np.array([[1, 0, 0], [1, 1, 1]])),
-    (colors.PURPLE, np.array([[0, 1, 0], [1, 1, 1]])),
-    (colors.CYAN, np.array([[1, 1, 1, 1]]))
-]
-
-
-class Board:
-    def __init__(self, BOARD_ROWS=20, BOARD_COLS=10):
-        self._board = np.zeros((BOARD_ROWS, BOARD_COLS), dtype=np.int)
-        self._board = np.random.uniform(low=0, high=8, size=(BOARD_ROWS, BOARD_COLS))
-        self._board = self._board.astype(np.int)
-
-    def apply_command(self, piece, curr_coords, commands):
-        """
-        :param piece: np.array
-        :param commands: [UP, DOWN, LEFT, RIGHT] bool
-        """
-        x, y = curr_coords
-        if commands[0]:  # UP
-            new_piece = np.rot90(piece)
-            piece_loc = np.zeros(self._board.shape, dtype=np.int)
-            piece_loc[y:new_piece.shape[1], x:new_piece.shape[0]] = new_piece[:, :]
-
-            if np.sum(self._board.astype(bool).astype(int) & piece_loc.astype(bool).astype(int)) == 0:  # no collision
-                piece = new_piece
-                self._board.astype(bool).astype(int) & piece_loc.astype(bool).astype(int)
-
-        elif commands[2]:  # LEFT
-            piece_loc = np.zeros(self._board.shape, dtype=np.int)
-            piece_loc[y:piece.shape[1], x:piece_loc.shape[0]] = piece_loc[:, :]
-
-            if np.sum(self._board.astype(bool).astype(int) & piece_loc.astype(bool).astype(int)) == 0:  # no collision
-                pass
-
-    def game_over(self):
-        return self._board[0].sum() != 0
-
-    def check_rows(self):
-        sums = np.argwhere(self._board.astype(bool).astype(int).sum(axis=1) == self._board.shape[1])
-        if len(sums) == 0:
-            return False
-
-        for row in reversed(range(self._board.shape[0])):
-            if np.sum(self._board[row].astype(bool).astype(int)) == self._board.shape[1]:
-                self._board[row, :] = np.zeros((1, self._board.shape[1]))
-
-            if row == self._board.shape[0] - 1:
-                continue
-
-            offset = 0
-            while offset + row + 1 < self._board.shape[0] and np.sum(self._board[row + 1 + offset]) == 0:
-                self._board[row + offset + 1, :] = self._board[row + offset, :]
-                self._board[row + offset, :] = np.zeros((1, self._board.shape[1]))
-                offset += 1
-        return True
-
-    def get_board(self):
-        return np.copy(self._board)
-
-    def render(self, screen: pygame.Surface):
-        for idx_r, row in enumerate(self._board):
-            for idx_c, col in enumerate(row):
-                color = PIECES[col - 1][0] if col != 0 else colors.BLACK
-                pygame.draw.rect(
-                    screen,
-                    color,
-                    pygame.Rect(
-                        (idx_c * BLOCK_SIZE, BLOCK_SIZE * idx_r),
-                        (BLOCK_SIZE, BLOCK_SIZE)
-                    )
-                )
-
-
-def get_inputs():
+def get_inputs_user() -> np.array:
     key = pygame.key.get_pressed()
-    return (key[K_UP], key[K_DOWN], key[K_LEFT], key[K_RIGHT])
+    return np.array([key[K_UP], key[K_DOWN], key[K_LEFT], key[K_RIGHT]])
 
 
 def main():
@@ -112,14 +34,13 @@ def main():
     while not board.game_over():
         tps = clock.tick(60)
         # fps = clock.get_fps()
-        inputs = get_inputs()
-        print(inputs)
+        inputs = get_inputs_user()
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 sys.exit()
 
         if time.time() - last_tick_time > TIME_BETWEEN_TICKS:
-            # board.tick_piece()
+            board.tick_piece()
             board.check_rows()
             board.render(screen)
             last_tick_time = time.time()
---
+++ test_board.py
@@ -0,0 +1,53 @@
+import unittest
+
+from board import Board, PIECES
+
+class TestBoard(unittest.TestCase):
+
+    def setUp(self):
+        self.board = Board()
+
+    def test_board_init(self):
+        self.assertEqual(self.board.get_board().sum(), 0)
+
+    def test_collides(self):
+        self.board._board[-1, :] = 1
+
+        piece = PIECES[0][1]
+        pos = [0, 18]
+        self.assertTrue(self.board.collides(piece, pos))
+        pos = [0, 17]
+        self.assertFalse(self.board.collides(piece, pos))
+
+    def test_out_of_bounds(self):
+        piece = PIECES[0][1]
+        pos = [0, 18]
+        self.assertFalse(self.board.out_of_bounds(piece, pos))
+        pos = [0, 19]
+        self.assertTrue(self.board.out_of_bounds(piece, pos))
+        pos = [-1, 18]
+        self.assertTrue(self.board.out_of_bounds(piece, pos))
+        pos = [19, 18]
+        self.assertTrue(self.board.out_of_bounds(piece, pos))
+        pos = [0, -1]
+        self.assertTrue(self.board.out_of_bounds(piece, pos))
+
+    def test_check_rows(self):
+        self.board._board[1:, :] = 1
+        self.assertTrue(self.board.check_rows())
+        self.assertEqual(self.board.get_board().sum(), 0)
+
+        self.board._board[0, 0] = 1
+        self.board._board[0, 1] = 2
+        self.board._board[1:, :] = 1
+        self.assertTrue(self.board.check_rows())
+        self.assertEqual(self.board.get_board().sum(), 3)
+        self.assertEqual(self.board.get_board()[-1, 1], 2)
+
+        self.board = Board()
+        self.assertEqual(self.board.get_board().sum(), 0)
+        self.assertFalse(self.board.check_rows())
+
+
+if __name__ == "__main__":
+    unittest.main()
